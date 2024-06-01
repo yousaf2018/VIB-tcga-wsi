d70e8355b78832c4865bc8456c8550e4c3f96f33
yousaf2018 <yousaf2018@namal.edu.pk>
2024-06-01 21:50:25 +0500

    Updated

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -147,47 +147,40 @@ def train(datasets, cur, args):
     if args.model_size is not None and args.model_type != 'mil':
         model_dict.update({"size_arg": args.model_size})
     
-    # if args.model_type in ['clam_sb', 'clam_mb']:
-    #     if args.subtyping:
-    #         model_dict.update({'subtyping': True})
+    if args.model_type in ['clam_sb', 'clam_mb']:
+        if args.subtyping:
+            model_dict.update({'subtyping': True})
         
-    #     if args.B > 0:
-    #         model_dict.update({'k_sample': args.B})
+        if args.B > 0:
+            model_dict.update({'k_sample': args.B})
         
-    #     if args.inst_loss == 'svm':
-    #         from topk.svm import SmoothTop1SVM
-    #         instance_loss_fn = SmoothTop1SVM(n_classes = 2)
-    #         if device.type == 'cuda':
-    #             instance_loss_fn = instance_loss_fn.cuda()
-    #     else:
-    #         instance_loss_fn = nn.CrossEntropyLoss()
+        if args.inst_loss == 'svm':
+            from topk.svm import SmoothTop1SVM
+            instance_loss_fn = SmoothTop1SVM(n_classes = 2)
+            if device.type == 'cuda':
+                instance_loss_fn = instance_loss_fn.cuda()
+        else:
+            instance_loss_fn = nn.CrossEntropyLoss()
 
-    #     if args.vib:
-    #         from models.vib_clam_wsi import CLAM_SB
-    #     else:
-    #         print('not use VIB')
-    #         from models.model_clam import CLAM_MB, CLAM_SB
+        if args.vib:
+            from models.vib_clam_wsi import CLAM_SB
+        else:
+            print('not use VIB')
+            from models.model_clam import CLAM_MB, CLAM_SB
 
-    #     if args.model_type =='clam_sb':
-    #         model = CLAM_SB(**model_dict, instance_loss_fn=instance_loss_fn)
-    #     elif args.model_type == 'clam_mb':
-    #         model = CLAM_MB(**model_dict, instance_loss_fn=instance_loss_fn)
-    #     else:
-    #         raise NotImplementedError
+        if args.model_type =='clam_sb':
+            model = CLAM_SB(**model_dict, instance_loss_fn=instance_loss_fn)
+        elif args.model_type == 'clam_mb':
+            model = CLAM_MB(**model_dict, instance_loss_fn=instance_loss_fn)
+        else:
+            raise NotImplementedError
     
-    # else: # args.model_type == 'mil'
-    #     if args.n_classes > 2:
-    #         model = MIL_fc_mc(**model_dict)
-    #     else:
-    #         model = MIL_fc(**model_dict)
-    # model.relocate()
-        
-    print('\nInit Model...', end=' ')
-    model = models.resnet18(True)
-    model.fc = nn.Linear(model.fc.in_features, args.n_classes)
-    model.cuda()
-    
-    print('Done!')
+    else: # args.model_type == 'mil'
+        if args.n_classes > 2:
+            model = MIL_fc_mc(**model_dict)
+        else:
+            model = MIL_fc(**model_dict)
+    model.relocate()
     print('Done!')
     print_network(model)
 

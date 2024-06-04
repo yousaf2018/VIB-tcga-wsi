ec68b9f75ffb7e9780dfd0dc06329e0aa487c889
yousaf2018 <mahmoodyousaf@neurog.ai>
2024-06-04 13:58:34 +0530

    udpated

--- vib_train.sh
+++ vib_train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=7 python /kaggle/working/WSI-finetuning/train_stage1.py --exp_code '/kaggle/working/clam_cam16_res50_vib_mse_infoloss_noavg' --vib
+CUDA_VISIBLE_DEVICES=7 python /kaggle/working/WSI-finetuning/train_stage1.py  --vib
